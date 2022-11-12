77c9c605cd4085d934ce8a9f6e09f38aa95a79e9
no nombre <smatamoro@uoc.edu>
2022-11-13 00:17:24 +0100

    producto2

---
+++ src/main/java/com/CodersTeam/ParaCasa/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.CodersTeam.ParaCasa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.CodersTeam.ParaCasa.entity.Menu;
+import com.CodersTeam.ParaCasa.repository.MenuRepository;
+
+
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	MenuRepository repositorioMenu;
+	
+	
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView mv=new ModelAndView("home");
+		return mv;
+	}
+	
+	
+	@GetMapping("/menus/seleccionMenu")
+	public ModelAndView SeleccionMenu() {
+		Iterable<Menu> menus= repositorioMenu.findAll();
+		ModelAndView mv=new ModelAndView("/views/menus/seleccionMenu");
+		mv.addObject("menus",menus);
+		return mv;
+	}
+	
+	@GetMapping("/menus/menuBebida")
+	public String SeleccionBebida() {
+		return "/views/menus/menuBebida";
+	}
+	
+	
+
+}
